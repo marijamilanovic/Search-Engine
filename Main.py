c4174b8f69aff6dde6561b599b8ab4cf2d0ca550
marijamilanovic <marijamil@uns.ac.rs>
2020-02-18 00:07:29 +0100

    #parsiranje_skupa_HTML_dokumenata parsira Trie stablo u proseku za 20 sekundi, potrebno ga je jos testirati...

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 
+from Parsiranje import *
+from time import time
+
+
+
 if __name__ == "__main__":
     running = True
     userInput = None
@@ -8,16 +13,28 @@ if __name__ == "__main__":
         print("-----------------------")
         print("MENU:")
         print("0 - Kraj")
-        print("1 - Parsiranje html fajla")
-        print("2 - proba")
+        print("1 - Parsiranje HTML fajlova iz test skupa")
+        print("2 - Parsiranje zadatih HTML fajlova")
+        print("3 - ")
 
         userInput = input("Izaberi broj opcije ->   ")
 
         if userInput == "1":
-            print("parse")
+            start_time = time()
+            parsiraj()
+            end_time = time()
+            ttime = end_time - start_time
+            print("Vreme parsiranja fajlova: " + str(ttime) + " sekudni.")
+
 
         if userInput == "2":
-            print("proba")
+            print("pr. C:\\Users\\MASHA\\test\\py-html")
+            myInput = str(input("Puna lokacija foldera ->  "))
+            startTime = time()
+            parsirajZadato(myInput)
+            endTime = time()
+            timee = endTime - startTime
+            print("Vreme parsiranja fajlova: " + str(timee) + " sekudni.")
 
         if userInput == "0":
                 running = False
--- Parsiranje.py
+++ Parsiranje.py
@@ -1,5 +1,38 @@
 from Parser import  *
+import os
+from Trie import TrieNode, add
 
 def parsiraj():
     root_dir = 'C:\\Users\\MASHA\\Desktop\\FAKS\\OISiSI\\MojDrugiProjekatMasha\\Projekat2\\test-skup\\python-2.7.7-docs-html'
     parser = Parser()
+    dictionary = {}                                                             # KLJUC = linkovi, VREDNOST = trie svih reci
+    #print("tu je")
+    for dir_path, dir_names, files in os.walk(root_dir):                        # "walking the tree"
+        #print("tuuu je")
+        for file in files:
+            if file.endswith(".html"):
+                root = TrieNode('')
+                path = os.path.abspath(dir_path + os.sep + file).lower()            # path.abspath -> Return a normalized absolutized version of the pathname path
+                words = parser.parse(path)[1]                                       # os.sep -> The character used by the operating system to separate pathname components
+                links = parser.parse(path)[0]                                       # MILICE TREBA TI ZA GRAF
+                for rec in words:
+                    add(root, rec.lower())
+                dictionary[path] = root
+                print(dictionary[path])
+
+
+
+def parsirajZadato(root_dir = str):
+    parser = Parser()
+    dictionary = {}                                                            # KLJUC = linkovi, VREDNOST = trie svih reci
+    for dir_path, dir_names, files in os.walk(root_dir):                       # "walking the tree"
+        for file in files:
+            if file.endswith(".html"):
+                root = TrieNode('')
+                path = os.path.abspath(dir_path + os.sep + file).lower()            # path.abspath -> Return a normalized absolutized version of the pathname path
+                words = parser.parse(path)[1]                                       # os.sep -> The character used by the operating system to separate pathname components
+                links = parser.parse(path)[0]                                       # MILICE TREBA TI ZA GRAF
+                for rec in words:
+                    add(root, rec.lower())
+                dictionary[path] = root
+                print(dictionary[path])
